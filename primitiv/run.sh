c1a707f24fcfb21d033f4b355c893371d29ac1b1
taku-y <taku.yoshioka.4096@gmail.com>
2019-03-11 01:38:28 +0900

    Build with Eigen3.3

--- primitiv/run.sh
+++ primitiv/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker run -it --rm \
     --volume $(pwd)/workspace:/root/workspace \
+    --volume $(pwd)/../../../primitiv/primitiv:/root/primitiv \
     --name my_primitiv my_primitiv
