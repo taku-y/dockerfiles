9d4fbbef4a71667934888e83259d579a9181cc74
taku-y <taku.yoshioka.4096@gmail.com>
2019-05-02 00:19:19 +0900

    Support tch-rs

--- learn-pp-rust/run.sh
+++ learn-pp-rust/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+dir_libtorch=/Users/taku-y/data/downloads/libtorch
+dir_tch_rs=$(pwd)/../../../LaurentMazare/tch-rs
+
 docker run -it --rm \
     --volume $(pwd)/../../../taku-y/primitiv:/root/primitiv \
     --volume $(pwd)/../../../taku-y/primitiv-rust:/root/primitiv-rust \
     --volume $(pwd)/learn-pp-rust:/root/learn-pp-rust \
+    --volume ${dir_libtorch}:/root/libtorch \
+    --volume ${dir_tch_rs}:/root/tch-rs \
     --name learn-pp-rust learn-pp-rust
