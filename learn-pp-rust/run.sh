4f638594417946bf358a11abc5f0c818d0b9b011
taku-y <taku.yoshioka.4096@gmail.com>
2019-05-12 15:49:11 +0900

    Add data directory

--- learn-pp-rust/run.sh
+++ learn-pp-rust/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dir_libtorch=/Users/taku-y/data/downloads/libtorch
 dir_tch_rs=$(pwd)/../../../LaurentMazare/tch-rs
+dir_tch_rs_data=/Users/taku-y/data/tch-rs-examples
 
 docker run -it --rm \
     --volume $(pwd)/../../../taku-y/primitiv:/root/primitiv \
@@ -8,4 +9,5 @@ docker run -it --rm \
     --volume $(pwd)/learn-pp-rust:/root/learn-pp-rust \
     --volume ${dir_libtorch}:/root/libtorch \
     --volume ${dir_tch_rs}:/root/tch-rs \
+    --volume ${dir_tch_rs_data}:/root/data \
     --name learn-pp-rust learn-pp-rust
