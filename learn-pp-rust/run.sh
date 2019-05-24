af034fa499846fd41d388160969391287b9e5b88
taku-y <taku.yoshioka.4096@gmail.com>
2019-05-25 02:25:08 +0900

    Minor changes

--- learn-pp-rust/run.sh
+++ learn-pp-rust/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dir_libtorch=/Users/taku-y/data/downloads/libtorch
+dir_libtorch=$HOME/data/downloads/libtorch
 dir_tch_rs=$(pwd)/../../../LaurentMazare/tch-rs
-dir_tch_rs_data=/Users/taku-y/data/tch-rs-examples
+dir_tch_rs_data=$HOME/data/tch-rs-examples
 
 docker run -it --rm \
     --volume $(pwd)/../../../taku-y/primitiv:/root/primitiv \
