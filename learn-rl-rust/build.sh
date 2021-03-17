8b6e130ecf6405d3f5109f76f651f7cbbe187ca8
taku-y <taku.yoshioka.4096@gmail.com>
2021-03-18 00:14:59 +0900

    Tweaks

--- learn-rl-rust/build.sh
+++ learn-rl-rust/build.sh
@@ -2,4 +2,5 @@
 docker build -t learn-rl-rust \
     --build-arg USER_ID=$(id -u) \
     --build-arg GROUP_ID=$(id -g) \
+    --build-arg GROUP=${GROUP:-${USER}} \
     --build-arg PASSWD="ubuntu" .
