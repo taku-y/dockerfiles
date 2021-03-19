2d0b4333bdee9e514883137105f7faeb45abe9b5
taku-y <taku.yoshioka.4096@gmail.com>
2021-03-19 23:05:43 +0900

    Tweak

--- learn-rl-rust/run.sh
+++ learn-rl-rust/run.sh
@@ -8,10 +8,10 @@
 #     learn-rl-rust
 
 docker run -it --rm \
+    --gpus 1 \
     -p 3389:3389 \
-    -p 22:22 \
     -u $(id -u):$(id -g) \
     -e USER=ubuntu \
     -e PASSWD=ubuntu \
-    -v /Users/taku-y/git/github/taku-y/learn-rl-rust:/home/ubuntu/lrr \
+    -v /Users/taku-y/git/github/taku-y/border:/home/ubuntu/border \
     learn-rl-rust
