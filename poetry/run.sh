d8277cf73de17d0021ebd99c5b1c7a00239d89c5
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-12-24 18:36:55 +0900

    WIP

---
+++ poetry/run.sh
@@ -0,0 +1,13 @@
+# docker run -it --rm \
+#     --gpus 1 \
+#     -e USER=ubuntu \
+#     -e PASSWD=ubuntu \
+#     -v $HOME/git/github/taku-y/border:/home/ubuntu/border \
+#     border
+
+docker run -it --rm \
+    -e USER=ubuntu \
+    -e PASSWD=ubuntu \
+    -v $HOME/git/github/taku-y/poetry:/home/ubuntu/poetry \
+    --name ptr \
+    poetry
