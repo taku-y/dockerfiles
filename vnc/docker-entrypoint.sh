8be27fd7a8aba1868f35a49a986da3d1a7cf4c85
taku-y <taku.yoshioka.4096@gmail.com>
2022-07-31 11:29:43 +0900

    Remove lines not used

--- vnc/docker-entrypoint.sh
+++ vnc/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 cd ~/
-pwd
-whoami
-ls -lh .vnc
 /bin/bash -c "echo -e 'password\npassword\nn' | vncpasswd"
 vncserver :1 -geometry 2560x1200 -depth 24
 websockify -D --web=/usr/share/novnc/ 80 localhost:5901
